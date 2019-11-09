3b97767f9256a21ee0259d63a27b9a298a1e157d
Philipp Schwerttraeger <ps@fkyp.de>
2019-11-09 18:39:23 +0100

    Remove Logo, Add Heading, Add Name to Header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -13,29 +13,46 @@ import {
   //OptionDiv,
   OptionLink,
 } from "./header.styles"
+import { clearEntries } from "../../redux/entries/entries.actions"
 
-const Header = ({ currentUser, hidden }) => (
-  <HeaderContainer>
-    <LogoContainer to="/">
-      <Logo className="logo" />
-    </LogoContainer>
-    <OptionsContainer>
-      <OptionLink as="div" onClick={() => alert()}>
-        SETTINGS
-      </OptionLink>
-      <OptionLink as="div" onClick={() => alert()}>
-        PROFILE
-      </OptionLink>
-      {currentUser ? (
-        <OptionLink as="div" onClick={() => auth.signOut()}>
-          SIGN OUT
-        </OptionLink>
-      ) : (
-        <OptionLink to="/signin">SIGN-IN</OptionLink>
-      )}
-    </OptionsContainer>
-  </HeaderContainer>
+// To use, use { HeaderLogo } in HeaderContainer below
+const HeaderLogo = (
+  <LogoContainer to="/">
+    <Logo className="logo" />
+  </LogoContainer>
 )
+
+const Header = ({ currentUser }) => {
+  const userName = currentUser.displayName
+    ? currentUser.displayName
+    : currentUser.email
+  return (
+    <HeaderContainer>
+      <h1>SIMPLE ACCOUNT MANAGER</h1>
+      <OptionsContainer>
+        <OptionLink as="div" onClick={() => alert()}>
+          SETTINGS
+        </OptionLink>
+        <OptionLink as="div" onClick={() => alert()}>
+          PROFILE
+        </OptionLink>
+        {currentUser ? (
+          <OptionLink
+            as="div"
+            onClick={() => {
+              clearEntries()
+              auth.signOut()
+            }}
+          >
+            SIGN {userName} OUT
+          </OptionLink>
+        ) : (
+          <OptionLink to="/signin">SIGN-IN</OptionLink>
+        )}
+      </OptionsContainer>
+    </HeaderContainer>
+  )
+}
 
 // question: why does selector know state? oO
 const mapStateToProps = createStructuredSelector({
