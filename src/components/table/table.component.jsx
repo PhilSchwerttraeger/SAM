b3e45718dd408b8b31226613eab194beafe6358b
Philipp Schwerttraeger <ps@fkyp.de>
2019-11-09 18:39:42 +0100

    Create simple table from fetched entries

--- src/components/table/table.component.jsx
+++ src/components/table/table.component.jsx
@@ -1,27 +1,13 @@
 import React from "react"
 import "./table.styles.scss"
 import { connect } from "react-redux"
-import { fetchEntriesStartAsync } from "../../redux/entries/entries.actions"
+import {
+  fetchEntriesStartAsync,
+  clearEntries,
+} from "../../redux/entries/entries.actions"
 import { createStructuredSelector } from "reselect"
 import { selectCurrentUser } from "../../redux/user/user.selectors"
-
-const entries = [
-  {
-    id: 0,
-    title: "title0",
-    url: "url0",
-  },
-  {
-    id: 1,
-    title: "title1",
-    url: "url1",
-  },
-  {
-    id: 2,
-    title: "title2",
-    url: "url2",
-  },
-]
+import { selectEntriesMap } from "../../redux/entries/entries.selectors"
 
 class Table extends React.Component {
   componentDidMount() {
@@ -29,23 +15,45 @@ class Table extends React.Component {
     fetchEntriesStartAsync(currentUser)
   }
 
+  componentWillUnmount() {
+    clearEntries()
+  }
+
   render() {
+    const { entries } = this.props
+    const entryIds = Object.keys(entries).map(e => e)
+    const entriesArray = entryIds.map(id => entries[id])
+    const entriesPropertiesArray = Object.keys(entriesArray[0]).map(e => e)
+    console.log(entriesArray)
+
+    //console.log(entries)
     return (
-      <div className="table">
-        {entries.map(({ id, url, title }) => (
-          <div className="row" key={id}>
-            <div>{id}</div>
-            <div>{title}</div>
-            <div>{url}</div>
-          </div>
-        ))}
-      </div>
+      <table className="table">
+        <thead>
+          <tr className="row">
+            {entriesPropertiesArray.map(property => (
+              <th key={property}>{property}</th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>
+          {entriesArray.map(entry => (
+            <tr key={entry.id} className="row">
+              {entriesPropertiesArray.map(property => (
+                <td key={property}>{entry[property]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     )
   }
 }
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
+  entries: selectEntriesMap,
 })
 
 const mapDispatchToProps = dispatch => ({
