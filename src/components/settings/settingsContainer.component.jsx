94d658972f0b935387550b81c8e2f597fc70d1b2
Philipp Schwerttraeger <ps@fkyp.de>
2020-02-05 20:24:40 +0100

    Add spinner to settings modal

--- src/components/settings/settingsContainer.component.jsx
+++ src/components/settings/settingsContainer.component.jsx
@@ -4,13 +4,18 @@ import {
   selectIsColumnsFetching,
   selectIsColumnsStoring,
 } from "../../redux/columns/columns.selectors"
+import WithSpinner from "../../components/with-spinner/with-spinner.component"
 import Settings from "./settings.component"
+import { compose } from "redux"
 
 const mapStateToProps = createStructuredSelector({
   isLoading: selectIsColumnsFetching || selectIsColumnsStoring,
 })
 
-const SettingsContainer = connect(mapStateToProps)(Settings)
+const SettingsContainer = compose(
+  connect(mapStateToProps), // second wrapper
+  WithSpinner, // first wrapper
+)(Settings)
 
 // Just passing down props, nothing to render here
 export default SettingsContainer
