7eb1d095715aa2be98c13105a1b1dc5e3f609c49
Philipp Schwerttraeger <ps@fkyp.de>
2020-02-10 14:00:25 +0100

    Add analysis calculation

--- src/components/analysis/analysis.component.jsx
+++ src/components/analysis/analysis.component.jsx
@@ -10,6 +10,37 @@ import { formatCurrencyToString } from "../tableEntries/tableEntries.utils"
 
 const Analysis = ({ visibleEntries, currentUser, entries, ...rest }) => {
   console.log(visibleEntries)
+  if (visibleEntries === null) return <></>
+  //console.log(visibleEntries)
+  const target = "value"
+  const visibleEntriesIn = visibleEntries.filter(
+    entry => entry.direction === "in",
+  )
+
+  const visibleEntriesOut = visibleEntries.filter(
+    entry => entry.direction === "out",
+  )
+
+  const length = visibleEntries.length
+  const lengthIn = visibleEntriesIn.length
+  const lengthOut = visibleEntriesOut.length
+
+  // SUM
+  let sumIn = 0
+  visibleEntriesIn.forEach(entry => {
+    sumIn = sumIn + entry[target]
+  })
+  let sumOut = 0
+  visibleEntriesOut.forEach(entry => {
+    sumOut = sumOut + entry[target]
+  })
+  let sum = sumIn - sumOut
+
+  // AVERAGE
+  let avg = length ? sum / length : "-"
+  let avgIn = lengthIn ? sumIn / lengthIn : "-"
+  let avgOut = lengthOut ? sumOut / lengthOut : "-"
+
 
   return (
     <AnalysisContainer>
@@ -24,12 +55,15 @@ const Analysis = ({ visibleEntries, currentUser, entries, ...rest }) => {
         <AnalysisMethodTitle>Stats</AnalysisMethodTitle>
         <div>
           <Left>Visible entries</Left>
+          <Right>{length ? length : 0}</Right>
         </div>
         <div>
           <Left>Visible entries (In)</Left>
+          <Right>{lengthIn ? lengthIn : 0}</Right>
         </div>
         <div>
           <Left>Visible entries (Out)</Left>
+          <Right>{lengthOut ? lengthOut : 0}</Right>
         </div>
       </Card>
       <Card>
@@ -38,12 +72,15 @@ const Analysis = ({ visibleEntries, currentUser, entries, ...rest }) => {
           <Left>
             All{" "}
           </Left>
+          <Right>{sum}</Right>
         </div>
         <div>
           <Left>In</Left>
+          <Right>{sumIn}</Right>
         </div>
         <div>
           <Left>Out</Left>
+          <Right>{sumOut}</Right>
         </div>
       </Card>
       <Card>
@@ -53,12 +90,15 @@ const Analysis = ({ visibleEntries, currentUser, entries, ...rest }) => {
             All{" "}
           </Left>
 
+          <Right>{avg}</Right>
         </div>
         <div>
           <Left>In</Left>
+          <Right>{avgIn}</Right>
         </div>
         <div>
           <Left>Out</Left>
+          <Right>{avgOut}</Right>
         </div>
       </Card>
     </AnalysisContainer>
